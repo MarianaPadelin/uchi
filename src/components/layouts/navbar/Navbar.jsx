e039550bca2768961841ff6f4eafd97e336f1ac7
MarianaPadelin <marianapadelin@gmail.com>
2023-09-08 17:56:11 -0300

    .

--- src/components/layouts/navbar/Navbar.jsx
+++ src/components/layouts/navbar/Navbar.jsx
@@ -14,7 +14,7 @@ import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 
 
-const pages = ["Products", "Pricing", "Blog"];
+const pages = ["Productos", "Servicios", "Blog"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const Navbar = () => {
