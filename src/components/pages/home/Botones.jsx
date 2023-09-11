9b7fa99236742e1b4a0ff693fe860fda55bd5f53
MarianaPadelin <marianapadelin@gmail.com>
2023-09-11 15:14:34 -0300

    link

--- src/components/pages/home/Botones.jsx
+++ src/components/pages/home/Botones.jsx
@@ -10,14 +10,14 @@ const images = [
     url: "https://d22fxaf9t8d39k.cloudfront.net/257be6d9ec51f8cf586e2f062cc3381c860d6a7e757edca56648c6e03de9457c58154.jpeg",
     title: "Productos",
     width: "40%",
-    path: "/productos",
+    path: "./productos"
   },
   {
     id: "2",
     url: "https://d22fxaf9t8d39k.cloudfront.net/248a2104cca292e92162b8ca298a93cd53fdc27cae07712e1f576f6f5d2a7aeb58154.jpg",
     title: "Servicios",
     width: "40%",
-    path: "/servicios"
+    path: "./servicios"
   },
   {
     id:"3",
